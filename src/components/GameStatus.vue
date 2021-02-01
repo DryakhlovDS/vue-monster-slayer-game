c7cc0276ff4aac54636bb219b98a39058bf5ca9d
DryakhlovDS <dryakhlovds@gmail.com>
2021-02-02 02:04:20 +0300

    restore file

---
+++ src/components/GameStatus.vue
@@ -0,0 +1,41 @@
+<template>
+  <section class="card-status">
+    <component :is="setSettings" @newSettings="newSettings"></component>
+  </section>
+</template>
+
+<script>
+import NewGame from './NewGame.vue';
+import Settings from './Settings.vue';
+
+export default {
+name: "GameStatus",
+components: {
+  NewGame,
+  Settings,
+},
+data: () => ({
+  setSettings: "NewGame",
+}),
+methods: {
+  newSettings(cmp){
+    this.setSettings = cmp;
+  },
+}
+}
+</script>
+
+<style lang="scss" scoped>
+.card-status{
+  width: 96%;
+  max-width: 600px;
+  margin: 2rem auto;
+  padding: 1rem;
+  border-radius: var(--border-radius);
+  border: 1px solid var(--color-shadow);
+  background-color: var(--color-bg);
+  color: var(--color-red);
+
+  
+}
+</style>
